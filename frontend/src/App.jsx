fdbb03d5f7443330a8f9aa17f5466f77f1a8e789
DianaSagoyan <49075763+DianaSagoyan@users.noreply.github.com>
2026-03-21 15:55:25 -0400

    Added React royer dom and added routes to Home and Books page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
 import Home from "./pages/Home";
+import Books from "./pages/Books";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<Books />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

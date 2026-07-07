a0374d58f30b9291f36a74a6d26b7d93548120a6
DianaSagoyan <49075763+DianaSagoyan@users.noreply.github.com>
2026-07-06 21:41:45 -0400

    handleSubmit method created

--- frontend/src/components/Quote_Form.jsx
+++ frontend/src/components/Quote_Form.jsx
@@ -37,4 +37,38 @@ export default function QuoteForm({ quote }) {
   const handleChange = (e) => {
     setForm(...form, { [e.target.name]: e.target.value });
   };
+
+  const handleSubmit = async () => {
+    if (!quote.text || !quote.book) {
+      setError("Quote and the name of the book are required");
+    }
+
+    const token = localStorage.getItem("token");
+    const url = isEditing
+      ? `${import.meta.env.VITE_API_URL}/quotes/${quote.id}`
+      : `${import.meta.env.VITE_API_URL}/quotes`;
+    const method = isEditing ? "PUT" : "POST";
+
+    try {
+      const res = await fetch(url, {
+        method,
+        headers: {
+          authorization: `Bearer ${token}`,
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(form),
+      });
+
+      if (!res.ok) {
+        throw new error(
+          isEditing ? "Quote coudn't be uodated" : "Quote can't be created",
+        );
+      }
+
+      const data = await res.json();
+      setForm(emptyForm);
+    } catch (err) {
+      setError(err.message);
+    }
+  };
 }

660c03f71d728aa1b907125baeeb8e370ec70428
DianaSagoyan <49075763+DianaSagoyan@users.noreply.github.com>
2026-09-12 15:36:50 -0400

    Added the "Add Book" modal

--- frontend/src/components/BookList.jsx
+++ frontend/src/components/BookList.jsx
@@ -6,6 +6,7 @@ import Form from "./Form";
 export default function BookList({ books, mode, onStartReading }) {
   const navigate = useNavigate();
   const [selectedBook, setSelectedBook] = useState(null);
+  const [showForm, setShowForm] = useState(false);
 
   // Color mapping for genres
   const genreColors = {
@@ -21,6 +22,10 @@ export default function BookList({ books, mode, onStartReading }) {
 
   const getColorForGenre = (genre) => {
     return genreColors[genre] || "#888888";
+  };
+
+  const openBookForm = () => {
+    setShowForm(true);
   };
 
   return (
@@ -59,6 +64,12 @@ export default function BookList({ books, mode, onStartReading }) {
           </li>
         ))}
       </ul> */}
+
+      <button className="add-book-btn" onClick={() => openBookForm()}>
+        Add Book
+      </button>
+
+      {/* {showForm && <Form />} */}
 
       <div className="books-grid">
         {books.map((book) => (
@@ -131,6 +142,17 @@ export default function BookList({ books, mode, onStartReading }) {
               X
             </button>
             <Form book={selectedBook} onSuccess={() => setSelectedBook(null)} />
+          </div>
+        </div>
+      )}
+
+      {showForm && (
+        <div className="modal-overlay">
+          <div className="modal-box">
+            <button className="modal-close" onClick={() => setShowForm(false)}>
+              X
+            </button>
+            <Form book={selectedBook} onSuccess={() => setShowForm(false)} />
           </div>
         </div>
       )}

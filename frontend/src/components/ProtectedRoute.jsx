f0dc11e8b6cf7acf291d95ee2f5336edba9a4429
DianaSagoyan <49075763+DianaSagoyan@users.noreply.github.com>
2026-09-14 16:59:37 -0400

    Fixed the logout logic inside navbar

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,7 +1,24 @@
-import { Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 
 export default function ProtectedRoute({children}){
     const token = localStorage.getItem("token");
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const handlePageShow = (event) => {
+            if(event.persisted){
+                const currentToken = localStorage.getItem("token");
+                if(!currentToken){
+                    window.location.href = "/login";
+                }
+            }
+        };
+
+        window.addEventListener("pageshow", handlePageShow);
+        return () => window.removeEventListener("pageshow", handlePageShow)
+
+    }, [navigate])
 
     if (!token) {
         return <Navigate to="/login" replace />;
